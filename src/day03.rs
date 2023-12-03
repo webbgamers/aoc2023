c1fcfec2872861f7d52b210b17b7578eea715bdd
Garrett Webb <webbgamers@gmail.com>
2023-12-03 13:17:11 -0500

    day 3 formatting

--- src/day03.rs
+++ src/day03.rs
@@ -1,68 +1,90 @@
-use std::fmt::Display;
 use regex::Regex;
 use std::collections::HashMap;
+use std::fmt::Display;
 
 pub fn solve(input: String) -> (String, String) {
-	(part1(&input).to_string(), part2(&input).to_string())
+    (part1(&input).to_string(), part2(&input).to_string())
 }
 
 fn part1(input: &str) -> impl Display {
-	let num_re = Regex::new(r"\d+").unwrap();
-	let sym_re = Regex::new(r"[\*/\+\%&\-=$@#]").unwrap();
+    let num_re = Regex::new(r"\d+").unwrap();
+    let sym_re = Regex::new(r"[\*/\+\%&\-=$@#]").unwrap();
 
-	let row_size = 141;
+    let row_size = 141;
 
-	let mut sum = 0;
+    let mut sum = 0;
 
-	let symbols = sym_re.find_iter(&input).map(|m| m.start() as isize ).collect::<Vec<_>>();
+    let symbols = sym_re
+        .find_iter(input)
+        .map(|m| m.start() as isize)
+        .collect::<Vec<_>>();
 
-	for part_num in num_re.find_iter(&input) {
-		if symbols.iter().any(|s| ((part_num.start() as isize - row_size - 1)..=(part_num.end() as isize - row_size)).contains(s) ) ||
-		symbols.iter().any(|s| ((part_num.start() as isize + row_size - 1)..=(part_num.end() as isize + row_size)).contains(s) ) ||
-		symbols.iter().any(|&s| s == part_num.start() as isize - 1) ||
-		symbols.iter().any(|&s| s == part_num.end() as isize) {
-			sum += part_num.as_str().parse::<i32>().unwrap();
-		}
-	}
+    for part_num in num_re.find_iter(input) {
+        if symbols.iter().any(|s| {
+            ((part_num.start() as isize - row_size - 1)..=(part_num.end() as isize - row_size))
+                .contains(s)
+        }) || symbols.iter().any(|s| {
+            ((part_num.start() as isize + row_size - 1)..=(part_num.end() as isize + row_size))
+                .contains(s)
+        }) || symbols.iter().any(|&s| s == part_num.start() as isize - 1)
+            || symbols.iter().any(|&s| s == part_num.end() as isize)
+        {
+            sum += part_num.as_str().parse::<i32>().unwrap();
+        }
+    }
 
-	sum
+    sum
 }
 
-
 fn part2(input: &str) -> impl Display {
-	let num_re = Regex::new(r"\d+").unwrap();
-	let sym_re = Regex::new(r"\*").unwrap();
+    let num_re = Regex::new(r"\d+").unwrap();
+    let sym_re = Regex::new(r"\*").unwrap();
 
-	let row_size = 141;
+    let row_size = 141;
 
-	let mut sum = 0;
+    let mut sum = 0;
 
-	let mut gears = sym_re.find_iter(&input).map(|m| (m.start() as isize, vec![]) ).collect::<HashMap<_,_>>();
+    let mut gears = sym_re
+        .find_iter(input)
+        .map(|m| (m.start() as isize, vec![]))
+        .collect::<HashMap<_, _>>();
 
-	for part_num in num_re.find_iter(&input) {
-		for n in (part_num.start() as isize - row_size - 1)..=(part_num.end() as isize - row_size) {
-			if gears.contains_key(&n) {
-				gears.get_mut(&n).unwrap().push(part_num.as_str().parse::<u32>().unwrap());
-			}
-		};
-		for n in (part_num.start() as isize + row_size - 1)..=(part_num.end() as isize + row_size) {
-			if gears.contains_key(&n) {
-				gears.get_mut(&n).unwrap().push(part_num.as_str().parse::<u32>().unwrap());
-			}
-		}
-		if gears.contains_key(&(part_num.start() as isize - 1)) {
-			gears.get_mut(&(part_num.start() as isize - 1)).unwrap().push(part_num.as_str().parse::<u32>().unwrap());
-		}
-		if gears.contains_key(&(part_num.end() as isize)) {
-			gears.get_mut(&(part_num.end() as isize)).unwrap().push(part_num.as_str().parse::<u32>().unwrap());
-		}
-	}
+    for part_num in num_re.find_iter(input) {
+        for n in (part_num.start() as isize - row_size - 1)..=(part_num.end() as isize - row_size) {
+            if gears.contains_key(&n) {
+                gears
+                    .get_mut(&n)
+                    .unwrap()
+                    .push(part_num.as_str().parse::<u32>().unwrap());
+            }
+        }
+        for n in (part_num.start() as isize + row_size - 1)..=(part_num.end() as isize + row_size) {
+            if gears.contains_key(&n) {
+                gears
+                    .get_mut(&n)
+                    .unwrap()
+                    .push(part_num.as_str().parse::<u32>().unwrap());
+            }
+        }
+        if gears.contains_key(&(part_num.start() as isize - 1)) {
+            gears
+                .get_mut(&(part_num.start() as isize - 1))
+                .unwrap()
+                .push(part_num.as_str().parse::<u32>().unwrap());
+        }
+        if gears.contains_key(&(part_num.end() as isize)) {
+            gears
+                .get_mut(&(part_num.end() as isize))
+                .unwrap()
+                .push(part_num.as_str().parse::<u32>().unwrap());
+        }
+    }
 
-	for (_,g) in gears {
-		if g.len() == 2 {
-			sum += g[0]*g[1]
-		}
-	}
+    for (_, g) in gears {
+        if g.len() == 2 {
+            sum += g[0] * g[1]
+        }
+    }
 
-	sum
+    sum
 }
